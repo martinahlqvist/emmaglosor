860db4a91e209a3a6197fa7a35cb03278d6aecb1
Ahlqvist <emma.ahlqvist@live.vasteras.se>
2019-05-14 11:17:08 +0200

    Fungerande version

--- js/glos.js
+++ js/glos.js
@@ -1,35 +1,344 @@
+
+
+const glosor = {
+    "glosa": [{
+        "sv": "Vad heter du?",
+        "fr": "Tu t'appelles comment?"
+    }, {
+        "sv": "Jag heter Emma",
+        "fr": "Je m'appelle Emma"
+    }, {
+        "sv": "Hur gammal är du?",
+        "fr": "Tu as quel âge?"
+    }, {
+        "sv": "Jag är tolv år",
+        "fr": "J'ai douze ans"
+    }, {
+        "sv": "Var bor du?",
+        "fr": "Yu habites où?"
+    }, {
+        "sv": "Jag bor i Nice",
+        "fr": "J'habite à Nice"
+    }, {
+        "sv": "Har du syskon?",
+        "fr": "TU as des frèes et soeurs?"
+    }, {
+        "sv": "Jag har en bror och en syster",
+        "fr": "J'ai un frères et une soeurs"
+    }, {
+        "sv": "Har du djur?",
+        "fr": "Tu as un animal?"
+    }, {
+        "sv": "Jag har en hund",
+        "fr": "J'ai un chien"
+    }, {
+        "sv": "Hur mår du?",
+        "fr": "Ca va?"
+    }, {
+        "sv": "Jag mår bra, tack",
+        "fr": "Ca va bien, merci"
+    }, {
+        "sv": "Tycker du om katter?",
+        "fr": "Tu aimes les chats?"
+    }, {
+        "sv": "Ja, jag tycker om katter",
+        "fr": "Oui, j'aime les chats"
+    }, {
+        "sv": "Jag tycker inte om sniglar",
+        "fr": "Je n'aime les chat"
+    }, {
+        "sv": "Hur mycket är klockan?",
+        "fr": "Quelle heure est-il?"
+    }, {
+        "sv": "Hon är tolv(på dagen)",
+        "fr": "Il est midi"
+    }, {
+        "sv": "Hon är tolv( på natten",
+        "fr": "Il est minuit"
+    }, {
+        "sv": "Hon är sex",
+        "fr": "Il est six heures"
+    }, {
+        "sv": "Hon är halv nio",
+        "fr": "Il est huit heures et demie"
+    }, {
+        "sv": "Mamma",
+        "fr": "Mère"
+    }, {
+        "sv": "Pappa",
+        "fr": "Père"
+    }, {
+        "sv": "Mormor",
+        "fr": "Grand-mère"
+    }, {
+        "sv": "Morfar",
+        "fr": "Grand-père"
+    }, {
+        "sv": "Syster",
+        "fr": "Soeur"
+    }, {
+        "sv": "Bror",
+        "fr": "Frère"
+    }, {
+        "sv": "Moster",
+        "fr": "Tante"
+    }, {
+        "sv": "Morbror",
+        "fr": "Oncle"
+    }, {
+        "sv": "Fru",
+        "fr": "Madame"
+    }, {
+        "sv": "Fröken",
+        "fr": "Mademoiselle"
+    }, {
+        "sv": "Herr",
+        "fr": "Monsieur"
+    }, {
+        "sv": "Pojke",
+        "fr": "Garcon"
+    }, {
+        "sv": "Flicka",
+        "fr": "Fille"
+    }, {
+        "sv": "Sverige",
+        "fr": "Suède"
+    }, {
+        "sv": "aaa",
+        "fr": "bbb"
+    }, {
+        "sv": "Svenska",
+        "fr": "Suèdois"
+    }, {
+        "sv": "Frankrike",
+        "fr": "France"
+    }, {
+        "sv": "Franska",
+        "fr": "Francais"
+    }, {
+        "sv": "Ost",
+        "fr": "Fromage"
+    }, {
+        "sv": "Skinka",
+        "fr": "Jambon"
+    }, {
+        "sv": "Smör",
+        "fr": "Beurre"
+    }, {
+        "sv": "Bröd",
+        "fr": "Pain"
+    }, {
+        "sv": "Ägg",
+        "fr": "Oeuf"
+    }, {
+        "sv": "Pastej",
+        "fr": "Pâtè"
+    }, {
+        "sv": "Mjölk",
+        "fr": "Lait"
+    }, {
+        "sv": "Juice",
+        "fr": "Jus"
+    }, {
+        "sv": "Sylt",
+        "fr": "Confiture"
+    }, {
+        "sv": "Vatten",
+        "fr": "Eau"
+    }, {
+        "sv": "Gul",
+        "fr": "Jaune"
+    }, {
+        "sv": "Grön",
+        "fr": "Vert"
+    }, {
+        "sv": "Blå",
+        "fr": "bleu"
+    }, {
+        "sv": "Röd",
+        "fr": "rouge"
+    }, {
+        "sv": "Vit",
+        "fr": "blanc"
+    }, {
+        "sv": "Svart",
+        "fr": "Noir"
+    }, {
+        "sv": "Grå",
+        "fr": "Gris"
+    }, {
+        "sv": "Brun",
+        "fr": "Marron"
+    }, {
+        "sv": "Rosa",
+        "fr": "Rose"
+    }, {
+        "sv": "Orange",
+        "fr": "Orange"
+    }, {
+        "sv": "Vad önskar ni?",
+        "fr": "Vous dèsirez?"
+    }, {
+        "sv": "Och som dryck?",
+        "fr": "Et comme boisson"
+    }, {
+        "sv": "Notan, Tack",
+        "fr": "L'addition, s'il vous plaît"
+    }, {
+        "sv": "Jag är hungrig",
+        "fr": "J'ai Faim"
+    }, {
+        "sv": "jag är törstig",
+        "fr": "J'ai soif"
+    }, {
+        "sv": "10",
+        "fr": "Dix"
+    }, {
+        "sv": "20",
+        "fr": "Vingt"
+    }, {
+        "sv": "30",
+        "fr": "Trente"
+    }, {
+        "sv": "40",
+        "fr": "Quarante"
+    }, {
+        "sv": "50",
+        "fr": "Cinquante"
+    }, {
+        "sv": "60",
+        "fr": "Soixante"
+    }, {
+        "sv": "70",
+        "fr": "Soixante-dix"
+    }, {
+        "sv": "80",
+        "fr": "Quatre-vingts"
+    }, {
+        "sv": "90",
+        "fr": "Quatre-vingt-dix"
+    }, {
+        "sv": "100",
+        "fr": "Cent"
+    }, {
+        "sv": "0",
+        "fr": "Zèro"
+    }, {
+        "sv": "1",
+        "fr": "un"
+    }, {
+        "sv": "2",
+        "fr": "Deux"
+    }, {
+        "sv": "3",
+        "fr": "Trois"
+    }, {
+        "sv": "4",
+        "fr": "Quatre"
+    }, {
+        "sv": "5",
+        "fr": "Cinq"
+    }, {
+        "sv": "6",
+        "fr": "Six"
+    }, {
+        "sv": "7",
+        "fr": "Sept"
+    }, {
+        "sv": "8",
+        "fr": "Huit"
+    }, {
+        "sv": "9",
+        "fr": "Neuf"
+    }, {
+        "sv": "10",
+        "fr": "Dix"
+    }, {
+        "sv": "11",
+        "fr": "Onze"
+    }, {
+        "sv": "12",
+        "fr": "Douze"
+    }, {
+        "sv": "13",
+        "fr": "Treize"
+    }, {
+        "sv": "14",
+        "fr": "Quatorze"
+    }, {
+        "sv": "15",
+        "fr": "Quinze"
+    }, {
+        "sv": "16",
+        "fr": "Seize"
+    }, {
+        "sv": "17",
+        "fr": "Dix-sept"
+    }, {
+        "sv": "18",
+        "fr": "Dix-huit"
+    }, {
+        "sv": "19",
+        "fr": "Dix-neuf"
+    }
+    ]
+}
+
+
+let nuvarandeGlosa = 0;
+let totaltAntalGlosor = glosor.glosa.length;
+var score = 0;
+
+
+function laddaGlosa(nummer) {
+    console.log("Nu ska vi skriva ut glosa " + nummer + "!!")
+    $("#introText").text('översätt glosa ' + nuvarandeGlosa);
+    $("#glosa").text(glosor.glosa[nummer].sv);
+}
+
+function kontrolleraSvar(svar, facit) {
+
+    console.log("Du har svarat '" + svar + "' rätt svar var '" + facit + "'.");
+
+    // Kontroller om svar är lika med facit om det är rätt så stega upp (addera 1 till nuvarande glosa och anroppa ladda glosa)
+
+
+    if (svar === facit) {
+        console.log("Rätt! 😄");
+        score++
+        nuvarandeGlosa++;
+
+        console.log("Nu har jag " + score + " rätt av " + totaltAntalGlosor);
+
+        laddaGlosa(nuvarandeGlosa);
+    } else {
+        console.log("Fel 😭");
+    }
+
+
+    $("#svarInput").val("");
+
+
+}
+
+
 $(document).ready(function () {
-
-    const glosor = {
-        "glosa": [{
-            "sv": "value1",
-            "fr": "value2"
-        }, {
-            "sv": "value3",
-            "fr": "value4"
-        }]
-    }
-
-
-    let nuvarandeGlosa=0;
-
-
-    function laddaGlosa(nummer){
-        $("#glosa").text(glosor.glosa[nummer].sv);
-    }
-
-    function kontrolleraSvar (svar, facit) {
-
-        console.log("Du har svarat '" + svar + "' rätt svar var '" + facit + "'.");
-
-// Kontroller om svar är lika med facit om det är rätt så stega upp (addera 1 till nuvarande glosa och anroppa ladda glosa)
-
-
-    }
-
-    console.log("ready!");
+    console.log("Nu är sidan laddad!! :(");
 
     laddaGlosa(nuvarandeGlosa);
+
+
+    $('#svarInput').keypress(function(event){
+       
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            event.preventDefault();
+            event.stopPropagation();
+            let svar = $("#svarInput").val();
+
+            kontrolleraSvar(svar, glosor.glosa[nuvarandeGlosa].fr);
+        }
+    });
 
 
 
@@ -39,7 +348,7 @@ $(document).ready(function () {
 
         kontrolleraSvar(svar, glosor.glosa[nuvarandeGlosa].fr);
 
-        $('#glosa').text(svar);
+
     });
 
 });
